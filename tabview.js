23a29785d94718fa05424a8b077b5a46e11a0e01
rananthan3 <rajan.ananthan@gmail.com>
2016-12-22 16:56:11 -0500

    Works Great

--- tabview.js
+++ tabview.js
@@ -1,11 +1,13 @@
 var tabee = {
 	view: "tabview",
-	width: 1500,	
+	width: 1500,
+	height: 5000,	
   	cells: [
     		{
       			header: "WSI Images",
       			body: {
-        			template: "Data Goes Here"        			
+						cols:[ {template:"Hello", width: 200}, {view: "template", content:"openseadragon1"}]
+        			       			
       			}
     		},
 			{
@@ -38,3 +40,17 @@ var tabee = {
       		}
 	]
 };
+
+$.getScript( "bower_components/openseadragon/built-openseadragon/openseadragon/openseadragon.min.js", function( data, textStatus, jqxhr ) {
+  		console.log( data ); // Data returned
+		//webix.ready();
+		var viewer = OpenSeadragon({
+        		id: "openseadragon1",
+        		prefixUrl: "/openseadragon/images/",
+        		tileSources: "logo.dzi"
+			});
+ }).fail(function( jqxhr, settings, exception ) {
+    $( "div.log" ).text( "Triggered ajaxError handler." );
+});
+
+
